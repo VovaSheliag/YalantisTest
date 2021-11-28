508d0c685efeac380cfc483069ca6d7d44e116b9
VovaSheliag <polar.caje@gmail.com>
2021-11-28 15:57:52 +0200

    add models to rest_parking app

--- car_park/rest_parking/models.py
+++ car_park/rest_parking/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    """
+    Model to save info about driver
+    """
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Vehicle(models.Model):
+    """
+    Model to save info about drivers vehicle
+    """
+    driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=128)
+    plate_number = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+
+    def __str__(self):
+        return f'{self.model} | {self.plate_number}'
+
+
+
+
