01091561879bd8e6119bbcd4b4d1cae7912c3772
VovaSheliag <polar.caje@gmail.com>
2021-11-28 17:01:58 +0200

    create Driver and Vehicle list serializers

--- car_park/rest_parking/serializers.py
+++ car_park/rest_parking/serializers.py
@@ -1,2 +1,16 @@
 from rest_framework import serializers
+from .models import Driver, Vehicle
+
+
+class DriversListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = '__all__'
+
+
+class VehicleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+
 
