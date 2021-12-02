acf6c38ca6dab50339f8680d01c7ea03ebbb9ca7
VovaSheliag <polar.caje@gmail.com>
2021-12-02 17:18:42 +0200

    Relocate some logic to services.py

---
+++ car_park/rest_parking/services.py
@@ -0,0 +1,31 @@
+from .models import Driver, Vehicle
+import datetime
+
+
+def get_drivers_list(request):
+    if 'created_at__gte' in request.GET:
+        date = get_splited_date(request.GET['created_at__gte'], '-')
+        # Get all drivers which were created gte request.created_at date
+        drivers_list = Driver.objects.filter(created_at__gte=datetime.datetime(date[2],
+                                                                               date[1], date[0]))
+    elif 'created_at__lte' in request.GET:
+        date = get_splited_date(request.GET['created_at__lte'], '-')
+        # Get all drivers which were created lte request.created_at date
+        drivers_list = Driver.objects.filter(created_at__lte=datetime.datetime(date[2],
+                                                                               date[1], date[0]))
+    else:
+        drivers_list = Driver.objects.all()
+    return drivers_list
+
+
+def get_splited_date(date, symbol):
+    """First argument is a date, and a second is a symbol which is used to split"""
+    return [int(x) for x in date.split(symbol)]
+
+
+def get_vehicles(request):
+    if 'with_drivers' in request.GET:
+        if request.GET['with_drivers'] == 'yes':
+            return Vehicle.objects.filter(driver_id=not None)
+        return Vehicle.objects.filter(driver_id=None)
+    return Vehicle.objects.all()
